246c2128914fdd481c8c53da8efe2bc6ed4b2d9c
NguyenXuanLoc <59056264+NguyenXuanLoc@users.noreply.github.com>
2022-08-18 21:52:12 +0700

    One trust.

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,13 @@ import 'dart:io' show Platform;
 import 'package:flutter/services.dart';
 import 'package:onetrust_publishers_native_cmp/onetrust_publishers_native_cmp.dart';
 
-
 void main() {
   runApp(const MaterialApp(title: "OneTrust Flutter Demo", home: HomePage()));
 }
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -23,6 +23,7 @@ class _HomePageState extends State<HomePage> {
   OTATTrackingAuthorizationStatus _attStatus =
       OTATTrackingAuthorizationStatus.notDetermined;
   String _dataSubjectId = "Unknown";
+
   @override
   void initState() {
     super.initState();
@@ -46,7 +47,8 @@ class _HomePageState extends State<HomePage> {
       return;
     }
     try {
-      status = await OTPublishersNativeSDK.startSDK("cdn.cookielaw.org", appId,'');
+      status =
+          await OTPublishersNativeSDK.startSDK("cdn.cookielaw.org", appId, '');
       if (await OTPublishersNativeSDK.shouldShowBanner()) {
         OTPublishersNativeSDK.showBannerUI();
       }
@@ -57,7 +59,7 @@ class _HomePageState extends State<HomePage> {
     }
 
     startupATTStatus =
-    await OTPublishersNativeSDK.getATTrackingAuthorizationStatus();
+        await OTPublishersNativeSDK.getATTrackingAuthorizationStatus();
 
     if (status! && shouldShowBanner!) {
       OTPublishersNativeSDK.showBannerUI();
@@ -74,8 +76,8 @@ class _HomePageState extends State<HomePage> {
 
   void startListening() {
     var consentListener =
-    OTPublishersNativeSDK.listenForConsentChanges(["C0003", "C0004"])
-        .listen((event) {
+        OTPublishersNativeSDK.listenForConsentChanges(["C0003", "C0004"])
+            .listen((event) {
       setCategoryState(event['categoryId'], event['consentStatus']);
       print("New status for " +
           event['categoryId'] +
@@ -84,7 +86,7 @@ class _HomePageState extends State<HomePage> {
     });
 
     var interactionListener =
-    OTPublishersNativeSDK.listenForUIInteractions().listen((event) {
+        OTPublishersNativeSDK.listenForUIInteractions().listen((event) {
       print(event);
     });
 
@@ -118,7 +120,7 @@ class _HomePageState extends State<HomePage> {
 
   void loadAgeGatePrompt() async {
     int? status =
-    await OTPublishersNativeSDK.showConsentUI(OTDevicePermission.ageGate);
+        await OTPublishersNativeSDK.showConsentUI(OTDevicePermission.ageGate);
     print("Age Gate Status is $status");
   }
 
@@ -141,35 +143,94 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text('OneTrust Download Status: $_cmpDownloadStatus\n'),
-            ElevatedButton(
-                onPressed: () {
-                  OTPublishersNativeSDK.showBannerUI();
-                },
-                child: Text("Load Banner")),
-            ElevatedButton(
-                onPressed: () {
-                  OTPublishersNativeSDK.showPreferenceCenterUI();
-                },
-                child: Text("Load Preference Center")),
-            ElevatedButton(
-                onPressed: () {
-                  loadAgeGatePrompt();
-                },
-                child: Text("Load Age Gate")),
-            Platform.isIOS //conditionally render ATT Pre-prompt button
-                ? getATTColumn()
-                : Container(),
-            Text('Category C0003 Status: $_c0003Status\n'),
-            Text('Category C0004 Status: $_c0004Status\n'),
-            Text('Data Subject Identifier is'),
-            Text(_dataSubjectId, style: TextStyle(fontWeight: FontWeight.bold)),
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Center(
+            child: Column(
+              children: [
+                Text('OneTrust Download Status: $_cmpDownloadStatus\n'),
+                ElevatedButton(
+                    onPressed: () {
+                      OTPublishersNativeSDK.showBannerUI();
+                    },
+                    child: Text("Load Banner")),
+                ElevatedButton(
+                    onPressed: () {
+                      OTPublishersNativeSDK.showPreferenceCenterUI();
+                    },
+                    child: Text("Load Preference Center")),
+                ElevatedButton(
+                    onPressed: () {
+                      loadAgeGatePrompt();
+                    },
+                    child: Text("Load Age Gate")),
+                Platform.isIOS //conditionally render ATT Pre-prompt button
+                    ? getATTColumn()
+                    : Container(),
+                Text('Category C0003 Status: $_c0003Status\n'),
+                Text('Category C0004 Status: $_c0004Status\n'),
+                Text('Data Subject Identifier is'),
+                Text(_dataSubjectId,
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+          )),
+          Stack(
+            children: [
+              SizedBox(height: 70),
+              Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    height: 70,
+                    color: Colors.yellow,
+                    child: Row(
+                      children: [
+                        itemBottomNavigationBar(),
+                        itemBottomNavigationBar(),
+                       const Expanded(child: SizedBox()),
+                        itemBottomNavigationBar(),
+                        itemBottomNavigationBar(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                  child: Align(
+                alignment: Alignment.topCenter,
+                child: Container(alignment: Alignment.bottomCenter,
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "sddsff",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text("sddsff", style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(100),
+                          topRight: Radius.circular(100))),
+                  width: 60,
+                  height: 90,
+                ),
+              ))
+            ],
+          )
+        ],
       ),
     );
   }
+
+  Widget itemBottomNavigationBar() => Expanded(
+          child: Column(mainAxisSize: MainAxisSize.min,
+        children: [Icon(Icons.add), Text('sdlfk')],
+      ));
 }
