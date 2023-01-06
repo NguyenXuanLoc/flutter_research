ddebcc18099b1983ba026115a24d587c42e4ba4d
DESKTOP-QCUULMR\Admin <locduoi1998>
2023-01-06 11:25:19 +0700

    IntrinsicWidth.

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage());
   }
 }
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -29,7 +29,65 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.center,
+          child: const IntrinsicWidth(
+            child: TextField(
+              maxLines: 1,
+              maxLength: 11,
+              textDirection: TextDirection.ltr,
+              keyboardType: TextInputType.number,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                prefixIconConstraints:
+                    BoxConstraints(maxWidth: 25, minWidth: 5, maxHeight: 70),
+                prefixIcon: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text('S/',
+                      style: TextStyle(
+                          color: Colors.deepPurpleAccent,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600)),
+                ),
+                counterText: "",
+                contentPadding: EdgeInsets.symmetric(horizontal: 0),
+                enabledBorder: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                hintText: "",
+                hintTextDirection: TextDirection.ltr,
+              ),
+              style: TextStyle(
+                color: Colors.deepPurpleAccent,
+                fontSize: 70,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget test(int second) {
+    return Wrap(
+      /*  mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,*/
+      direction: Axis.horizontal,
+      children: [
+        const Text(
+          "s/",
+          style:
+              TextStyle(fontSize: 40, fontWeight: FontWeight.w600, height: 1),
+        ),
+        TextFormField()
+        /*  Text(
+          "$second",
+          style: const TextStyle(
+              fontSize: 100, fontWeight: FontWeight.w600, height: 1),
+        )*/
+      ],
+    );
   }
 }
-
